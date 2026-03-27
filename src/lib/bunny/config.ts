0a6199891695d3303e1794c40261b895e20bedb7
Abdelrahman <dodoeid1980@gmail.com>
2026-03-27 16:26:58 +0200

    chore: update bunny.net credentials

--- src/lib/bunny/config.ts
+++ src/lib/bunny/config.ts
@@ -1,6 +1,6 @@
 export const BUNNY_CONFIG = {
-    STORAGE_ZONE_NAME: process.env.BUNNY_STORAGE_ZONE_NAME || "manara-academy",
-    ACCESS_KEY: process.env.BUNNY_ACCESS_KEY || "68026312-9874-4b2d-9cc6b9ce586f-14dc-4a44",
-    STORAGE_ENDPOINT: process.env.BUNNY_STORAGE_ENDPOINT || "uk.storage.bunnycdn.com",
-    PULL_ZONE_URL: process.env.NEXT_PUBLIC_BUNNY_PULL_ZONE_URL || "https://manara-academy.b-cdn.net"
+    STORAGE_ZONE_NAME: process.env.BUNNY_STORAGE_ZONE_NAME || "manar-aacademy",
+    ACCESS_KEY: process.env.BUNNY_ACCESS_KEY || "3c738937-d1a7-42fe-867570f4a92d-f87d-4ff4",
+    STORAGE_ENDPOINT: process.env.BUNNY_STORAGE_ENDPOINT || "storage.bunnycdn.com",
+    PULL_ZONE_URL: process.env.NEXT_PUBLIC_BUNNY_PULL_ZONE_URL || "https://manaraacademy.b-cdn.net"
 };
